f7a15ab84c53e976c583cb89de3cf2b8352d5e48
lily520999 <kaijiarui2018@gmail.com>
2025-01-17 20:44:36 +0800

    Clean up and reorganize files

--- MainActivity.java
+++ MainActivity.java
@@ -1,1 +1,17 @@
- 
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        webView = findViewById(R.id.webview);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setAllowFileAccess(true);
+        webView.setWebChromeClient(new WebChromeClient());
+        
+        // 加载本地 HTML
+        webView.loadUrl("file:///android_asset/index.html");
+    }
+} 
